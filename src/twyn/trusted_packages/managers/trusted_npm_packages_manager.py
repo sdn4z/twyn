32092a9bef757bfcd5f8945eeaa8e53a57714579
Daniel Sanz <13658011+sdn4z@users.noreply.github.com>
2025-10-30 10:59:03 +0100

    wip

--- src/twyn/trusted_packages/managers/trusted_npm_packages_manager.py
+++ src/twyn/trusted_packages/managers/trusted_npm_packages_manager.py
@@ -13,25 +13,6 @@ from twyn.trusted_packages.models import TyposquatCheckResultEntry
 from twyn.trusted_packages.selectors import AbstractSelector
 
 
-@dataclass
-class NamespaceDependency:
-    package: str
-    namespace: str | None = None
-
-    @property
-    def full_name(self) -> str:
-        if self.namespace:
-            return f"{self.namespace}/{self.package}"
-        return self.package
-
-    @classmethod
-    def from_name(cls, name: str) -> Self:
-        if name.startswith("@"):
-            namespace, package = name.split("/")
-            return cls(package=package, namespace=namespace)
-        return cls(package=name)
-
-
 class TrustedNpmPackageManager:
     """Representation of namespaces that can be trusted."""
 
@@ -59,33 +40,29 @@ class TrustedNpmPackageManager:
         first_letter_names: OrderedPackages = defaultdict(set)
         namespaces: OrderedPackages = defaultdict(set)
         for name in names:
-            dependency = NamespaceDependency.from_name(name)
-            if dependency.namespace:
-                if dependency.namespace not in namespaces:
-                    namespaces[dependency.namespace] = set()
-                namespaces[dependency.namespace].add(dependency.package)
+            if name.startswith("@"):
+                namespace, dependency = name.split("/")
+                if namespace not in namespaces:
+                    namespaces[namespace] = set()
+                namespaces[namespace].add(dependency)
             else:
-                first_letter_names[name[0]].add(dependency.package)
+                first_letter_names[name[0]].add(name)
         return first_letter_names, namespaces
 
-    def _get_typosquats_from_namespace_dependency(self, dependency: NamespaceDependency) -> Any:
-        threshold = self.threshold_class.from_name(dependency.namespace)
-        typosquat_result = TyposquatCheckResultEntry(dependency=dependency.full_name)
-        for trusted_namespace_name in self.selector.select_similar_names(
-            names=self.namespaces.keys(), name=dependency.namespace
-        ):
-            distance = self.algorithm.get_distance(dependency.namespace, trusted_namespace_name)
-            if (
-                threshold.is_inside_threshold(distance)
-                and dependency.package in self.namespaces[trusted_namespace_name]
-            ):
+    def _get_typosquats_from_namespace_dependency(self, package_name: str) -> Any:
+        namespace, dependency = package_name.split("/")
+        threshold = self.threshold_class.from_name(namespace)
+        typosquat_result = TyposquatCheckResultEntry(dependency=package_name)
+        for trusted_namespace_name in self.selector.select_similar_names(names=self.namespaces.keys(), name=namespace):
+            distance = self.algorithm.get_distance(namespace, trusted_namespace_name)
+            if threshold.is_inside_threshold(distance) and dependency in self.namespaces[trusted_namespace_name]:
                 typosquat_result.add(trusted_namespace_name)
 
-    def _get_typosquats_from_dependency(self, dependency: NamespaceDependency) -> Any:
-        threshold = self.threshold_class.from_name(dependency.package)
-        typosquat_result = TyposquatCheckResultEntry(dependency=dependency.package)
-        for trusted_package_name in self.selector.select_similar_names(names=self.packages, name=dependency.package):
-            distance = self.algorithm.get_distance(dependency.package, trusted_package_name)
+    def _get_typosquats_from_dependency(self, package_name: str) -> Any:
+        threshold = self.threshold_class.from_name(package_name)
+        typosquat_result = TyposquatCheckResultEntry(dependency=package_name)
+        for trusted_package_name in self.selector.select_similar_names(names=self.packages, name=package_name):
+            distance = self.algorithm.get_distance(package_name, trusted_package_name)
             if threshold.is_inside_threshold(distance):
                 typosquat_result.add(trusted_package_name)
         return typosquat_result
@@ -97,7 +74,6 @@ class TrustedNpmPackageManager:
         considered similar to another one. The algorithm provided and the threshold
         are used to determine if the package name can be considered similar.
         """
-        dependency = NamespaceDependency.from_name(package_name)
-        if dependency.namespace:
-            return self._get_typosquats_from_namespace_dependency(dependency)
-        return self._get_typosquats_from_dependency(dependency)
+        if package_name.startswith("@"):
+            self._get_typosquats_from_namespace_dependency(package_name)
+        return self._get_typosquats_from_dependency(package_name)
